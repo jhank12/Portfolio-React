6f7658a354827eabf7965fc8138fd182f480dcf1
Josh Hankins <jhankins0903@gmail.com>
2023-12-18 14:53:09 -0500

    added smooth scroll in project section

--- src/Components/Sections/ProjectsSection.jsx
+++ src/Components/Sections/ProjectsSection.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import "/src/projectsSection.css";
 import ImageGallery from "../ImageGallery/ImageGallery";
 
 const ProjectsSection = () => {
+  const calendarRef = useRef(null);
+  const cartRef = useRef(null);
+  const calculatorRef = useRef(null);
+  const pixelArtRef = useRef(null);
+
   const testArr = [
     {
       name: "Calendar",
@@ -16,9 +21,12 @@ const ProjectsSection = () => {
         "../src/Images/CalendarImages/calendar_signup1.PNG",
         "../src/Images/CalendarImages/calendar_forgotpassword1.PNG",
       ],
+      description:
+        "Users can create an account and add custom events to the calendar. Any changes to the calendar are saved and updated in realtime with the Firestore database.",
       tech: ["React", "Redux Toolkit", "Firebase"],
       site: "https://calendar-jhank12.netlify.app/#/",
       code: "https://github.com/jhank12/Calendar",
+      ref: calendarRef,
     },
     {
       name: "Shopping Cart",
@@ -29,10 +37,13 @@ const ProjectsSection = () => {
         "../src/Images/ShoppingCart/cart_2.PNG",
         "../src/Images/ShoppingCart/cart_checkout.PNG",
       ],
+      description:
+        "Fake e-commerce site with products that users can choose a quantity of and add to their cart.",
       tech: ["React", "TypeScript", "Redux Toolkit"],
 
       site: "https://jhank12-shopping-cart.netlify.app/",
       code: "https://github.com/jhank12/Shopping-Cart",
+      ref: cartRef,
     },
     {
       name: "Tip Calculator",
@@ -41,10 +52,14 @@ const ProjectsSection = () => {
         "../src/Images/TipCalculator/calculator_main.PNG",
         "../src/Images/TipCalculator/calculator_2.PNG",
       ],
+      description:
+        "Allows users to input bill amount, number of people and tip percentage to divide the bill evenly.",
+
       tech: ["HTML", "CSS", "JavaScript"],
 
       site: "https://jhank12.github.io/Tip-Calculator/",
       code: "https://github.com/jhank12/Tip-Calculator",
+      ref: calculatorRef,
     },
     {
       name: "Pixel Art Editor",
@@ -54,14 +69,41 @@ const ProjectsSection = () => {
         "../src/Images/PixelArtEditor/editor2.PNG",
         "../src/Images/PixelArtEditor/editor3.PNG",
       ],
+      description:
+        "Fake e-commerce site with products that users can choose a quantity of and add to their cart.",
+
       tech: ["HTML", "CSS", "JavaScript"],
 
       site: "https://jhank12.github.io/Pixel-Art-Editor/",
       code: "https://github.com/jhank12/Pixel-Art-Editor",
+      ref: pixelArtRef,
     },
   ];
 
   const [link, setLink] = useState();
+
+  let ref = useRef(null);
+
+  function handleScroll(id) {
+    console.log(id);
+    switch (id) {
+      case "calendar":
+        ref = calendarRef;
+        break;
+      case "shoppingCart":
+        ref = cartRef;
+        break;
+
+      case "calculator":
+        ref = calculatorRef;
+        break;
+      case "pixelArt":
+        ref = pixelArtRef;
+        break;
+    }
+
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }
 
   return (
     <section className="contentSection">
@@ -69,21 +111,36 @@ const ProjectsSection = () => {
 
       <div className="projectsContainer">
         {/* left side navigation */}
-        {/* <nav className="projectsNav">
+        <nav className="projectsNav">
+          {/* render dynamically from arr using id */}
           <ul>
-            <li onClick={() => setLink("calendar")}>Calendar</li>
-            <li onClick={() => setLink("shoppingCart")}>E-Commerce</li>
-            <li onClick={() => setLink("calculator")}>Tip Calculator</li>
-            <li onClick={() => setLink("pixelArt")}>Pixel Art Editor</li>
+            {testArr.map((item) => {
+              const { name, id } = item;
+
+              return (
+                <li>
+                  <a onClick={() => handleScroll(id)}>{name}</a>
+                </li>
+              );
+            })}
           </ul>
-        </nav> */}
+        </nav>
 
         <div className="projectsList">
           {testArr.map((project) => {
-            const { name, id, images, tech: techArr, site, code } = project;
+            const {
+              name,
+              id,
+              images,
+              description,
+              tech: techArr,
+              site,
+              code,
+              ref,
+            } = project;
 
             return (
-              <div key={id} className="project" id={id}>
+              <div key={id} className="project" id={id} ref={ref}>
                 <div>
                   <h2>{name}</h2>
                 </div>
@@ -91,26 +148,29 @@ const ProjectsSection = () => {
                 <ImageGallery images={images} />
 
                 <div className="projectDescription">
-                  <h4>Description</h4>
-                  <br />
-                  <p className="font-secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Culpa nemo aliquid, modi deleniti enim saepe. Nisi
-                    blanditiis sequi non ipsum debitis maxime omnis eveniet
-                    expedita?
-                  </p>
+                  {/* <h4>Description</h4> */}
+                  {/* <br /> */}
+                  <p className="font-secondary">{description}</p>
                 </div>
 
                 <p className="gray-dark">
-                  Technology used: {" "}
+                  Technology used:{" "}
                   {techArr.map((tech) => {
-                    return <strong key={tech}>{tech}, {" "}</strong> ;
+                    if (techArr.indexOf(tech) !== techArr.length - 1) {
+                      return <strong key={tech}> {tech}, </strong>;
+                    } else {
+                      return <strong key={tech}> {tech} </strong>;
+                    }
                   })}
                 </p>
 
                 <div className="projectActions">
-                  <button>Live Site</button>
-                  <a href={site} rel="noreferrer" target="_blank">
+                  <a
+                    className="linkBtn"
+                    href={site}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
                     Live Site
                   </a>
                   <a href={code} rel="noreferrer" target="_blank">
