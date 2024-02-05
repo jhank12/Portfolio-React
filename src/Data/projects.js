10d2abe9d1f354e6fa4b8c518064204eb9ea7061
Josh Hankins <jhankins0903@gmail.com>
2024-02-05 17:14:13 -0500

    commit

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -3,12 +3,12 @@ export const projectsArr = [
     name: "Calendar",
     id: "calendar",
     images: [
-      "../../src/Images/CalendarImages/calendar_main2.JPG",
-      "../src/Images/CalendarImages/calendar_eventview1.PNG",
-      "../src/Images/CalendarImages/calendar_addEvent1.JPG",
-      "../src/Images/CalendarImages/calendar_login1.PNG",
-      "../src/Images/CalendarImages/calendar_signup1.PNG",
-      "../src/Images/CalendarImages/calendar_forgotpassword1.PNG",
+      "../../Images/CalendarImages/calendar_main2.JPG",
+      "../../Images/CalendarImages/calendar_eventview1.PNG",
+      "../../Images/CalendarImages/calendar_addEvent1.JPG",
+      "../../Images/CalendarImages/calendar_login1.PNG",
+      "../../Images/CalendarImages/calendar_signup1.PNG",
+      "../../Images/CalendarImages/calendar_forgotpassword1.PNG",
     ],
     description:
       "This is a calendar application that allows users to create custom events. All events have CRUD functionality and any changes are updated in real time on a Firestore Database. All accounts are created using Firebase Authentication.",
@@ -20,10 +20,10 @@ export const projectsArr = [
     name: "Shopping Cart",
     id: "shoppingCart",
     images: [
-      "../src/Images/ShoppingCart/cart_main2.PNG",
-      "../src/Images/ShoppingCart/cart_cartModal.PNG",
-      "../src/Images/ShoppingCart/cart_2.PNG",
-      "../src/Images/ShoppingCart/cart_checkout.PNG",
+      "../../Images/ShoppingCart/cart_main2.PNG",
+      "../../Images/ShoppingCart/cart_cartModal.PNG",
+      "../../Images/ShoppingCart/cart_2.PNG",
+      "../../Images/ShoppingCart/cart_checkout.PNG",
     ],
     description:
       "Fake e-commerce site with products that users can choose a quantity of and add to their cart.",
@@ -36,8 +36,8 @@ export const projectsArr = [
     name: "Tip Calculator",
     id: "calculator",
     images: [
-      "../src/Images/TipCalculator/calculator_main.PNG",
-      "../src/Images/TipCalculator/calculator_2.PNG",
+      "../../Images/TipCalculator/calculator_main.PNG",
+      "../../Images/TipCalculator/calculator_2.PNG",
     ],
     description:
       "Allows users to input bill amount, number of people and tip percentage to divide the bill evenly.",
@@ -52,9 +52,9 @@ export const projectsArr = [
     name: "Pixel Art Editor",
     id: "pixelArt",
     images: [
-      "../src/Images/PixelArtEditor/editor_main.PNG",
-      "../src/Images/PixelArtEditor/editor2.PNG",
-      "../src/Images/PixelArtEditor/editor3.PNG",
+      "../../Images/PixelArtEditor/editor_main.PNG",
+      "../../Images/PixelArtEditor/editor2.PNG",
+      "../../Images/PixelArtEditor/editor3.PNG",
     ],
     description:
       " Simple pixel art editor that allows users to select a grid size and draw pixel art. The selected grid size is calculated with JavaScript and displayed using the HTML Canvas element",
