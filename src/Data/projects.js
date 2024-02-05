1469e399ae6f29dbe6a68c8da97b8281649f5b7d
Josh Hankins <jhankins0903@gmail.com>
2024-02-05 18:01:44 -0500

    commit

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,19 +1,21 @@
+import calendarImg1 from "../Images/CalendarImages/calendar_main2.JPG";
+import calendarImg2 from "../Images/CalendarImages/calendar_eventview1.PNG";
+import calendarImg3 from "../Images/CalendarImages/calendar_addEvent1.JPG";
+import calendarImg4 from "../Images/CalendarImages/calendar_login1.PNG";
+import calendarImg5 from "../Images/CalendarImages/calendar_signup1.PNG";
+import calendarImg6 from "../Images/CalendarImages/calendar_forgotpassword1.PNG";
 
-import calendarImg1 from "../Images/calendar_main2.JPG";
-import calendarImg2 from "../Images/calendar_eventview1.PNG";
-import calendarImg3 from "../Images/calendar_addEvent1.JPG";
-import calendarImg4 from "../Images/calendar_login1.PNG";
-import calendarImg5 from "../Images/calendar_signup1.PNG";
-import calendarImg6 from "../Images/calendar_forgotpassword1.PNG";
+import shoppingCartImg1 from "../Images/ShoppingCart/cart_main2.PNG";
+import shoppingCartImg2 from "../Images/ShoppingCart/cart_cartModal.PNG";
+import shoppingCartImg3 from "../../Images/ShoppingCart/cart_2.PNG";
+import shoppingCartImg4 from "../../Images/ShoppingCart/cart_checkout.PNG";
 
-// images: [
-//   "../Images/calendar_main2.JPG",
-//   "../Images/calendar_eventview1.PNG",
-//   "../Images/calendar_addEvent1.JPG",
-//   "../Images/calendar_login1.PNG",
-//   "../Images/calendar_signup1.PNG",
-//   "../Images/calendar_forgotpassword1.PNG",
-// ],
+import tipCalculatorImg1 from "../Images/TipCalculator/calculator_main.PNG";
+import tipCalculatorImg2 from "../Images/TipCalculator/calculator_2.PNG";
+
+import pixelEditorImg1 from "../Images/PixelArtEditor/editor_main.PNG";
+import pixelEditorImg2 from "../Images/PixelArtEditor/editor2.PNG";
+import pixelEditorImg3 from "../Images/PixelArtEditor/editor3.PNG";
 
 export const projectsArr = [
   {
@@ -26,7 +28,6 @@ export const projectsArr = [
       `${calendarImg4}`,
       `${calendarImg5}`,
       `${calendarImg6}`,
-
     ],
     description:
       "This is a calendar application that allows users to create custom events. All events have CRUD functionality and any changes are updated in real time on a Firestore Database. All accounts are created using Firebase Authentication.",
@@ -38,10 +39,10 @@ export const projectsArr = [
     name: "Shopping Cart",
     id: "shoppingCart",
     images: [
-      "../../Images/ShoppingCart/cart_main2.PNG",
-      "../../Images/ShoppingCart/cart_cartModal.PNG",
-      "../../Images/ShoppingCart/cart_2.PNG",
-      "../../Images/ShoppingCart/cart_checkout.PNG",
+      `${shoppingCartImg1}`,
+      `${shoppingCartImg2}`,
+      `${shoppingCartImg3}`,
+      `${shoppingCartImg4}`,
     ],
     description:
       "Fake e-commerce site with products that users can choose a quantity of and add to their cart.",
@@ -53,10 +54,7 @@ export const projectsArr = [
   {
     name: "Tip Calculator",
     id: "calculator",
-    images: [
-      "../../Images/TipCalculator/calculator_main.PNG",
-      "../../Images/TipCalculator/calculator_2.PNG",
-    ],
+    images: [`${tipCalculatorImg1}`, `${tipCalculatorImg2}`],
     description:
       "Allows users to input bill amount, number of people and tip percentage to divide the bill evenly.",
 
@@ -69,11 +67,7 @@ export const projectsArr = [
   {
     name: "Pixel Art Editor",
     id: "pixelArt",
-    images: [
-      "../../Images/PixelArtEditor/editor_main.PNG",
-      "../../Images/PixelArtEditor/editor2.PNG",
-      "../../Images/PixelArtEditor/editor3.PNG",
-    ],
+    images: [`${pixelEditorImg1}`, `${pixelEditorImg2}`, `${pixelEditorImg3}`],
     description:
       " Simple pixel art editor that allows users to select a grid size and draw pixel art. The selected grid size is calculated with JavaScript and displayed using the HTML Canvas element",
 
