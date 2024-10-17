e6212411a353d7e5546eb8da26d85cf62897f418
Joshua Hankins <jhankins0903@gmail.com>
2024-10-17 10:01:37 -0400

    changed font

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
         <LandingSection scrollToElement={scrollToElement} />
 
         <SkillsSection />
-        <h1>adsfasdf</h1>
         <ProjectsSection />
       </section>
 
